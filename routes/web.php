451c165445393a077144a4f55a61b51db74a5fe8
omerHany <hanyomar367@gmail.com>
2023-03-15 14:03:29 +0200

    12

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::prefix('/admin')->middleware('auth:admin')->group(function () {
 
         Route::resource('/clubs', AdminController::class,);
          
-        Route::view('/تعديل_الاخبار','new.newEdit')->name('news');
+          Route::view('/تعديل_الاخبار','new.newEdit')->name('news');
     });
     // dashboard
 Route::view('dashboard', 'parant')->name('dashboard');
